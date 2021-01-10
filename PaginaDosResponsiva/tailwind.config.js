4d344ec4fedfe1d7746239052bfb318a1a17165a
afic14 <afc.intriago@yavirac.edu.ec>
2021-01-09 19:32:39 -0500

    Segunda página con tailwindcss

---
+++ PaginaDosResponsiva/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container:{
+      center:true,
+    },
+    extend: {
+      colors :{
+        primary: '#1a202c',
+        secondary: '#161b25',
+        text_primary: '#f7fafc',
+        text_secondary: '#e2e8f0',
+        text_tertiary: '#a0aec0',
+        veryblack: '#000000',
+        main: '#F7F8F9',
+        footer: '#2A2A2A',
+        etiquetas: '#ECC731',
+        fondocarrucel: '#8636B2'
+      }
+    }
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
